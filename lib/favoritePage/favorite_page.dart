f27787ead54b48a4ae154d8a50a212fb04d1e42c
ZykX <Zikrinaya05@gmail.com>
2024-05-06 07:06:28 +0700

    feat [MZ] add notification page

--- lib/favoritePage/favorite_page.dart
+++ lib/favoritePage/favorite_page.dart
@@ -19,23 +19,21 @@ class FavoritePage extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return FavoriteCard(
-                imagePath: 'https://via.placeholder.com/50x50',
-                title:
-                    'Product uadnadiadiadiwd awujdniandiandadjnwa iadniandandaikd aikujdnaidnad',
-                price: 12000,
-                rating: (index % 5) + 1,
-                profileImagePath: 'https://via.placeholder.com/20x20',
-                profileUsername: 'Username Toko',
-              );
-            },
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return FavoriteCard(
+              imagePath: 'https://via.placeholder.com/50x50',
+              title:
+                  'Product uadnadiadiadiwd awujdniandiandadjnwa iadniandandaikd aikujdnaidnad',
+              price: 12000,
+              rating: (index % 5) + 1,
+              profileImagePath: 'https://via.placeholder.com/20x20',
+              profileUsername: 'Username Toko',
+            );
+          },
         ),
       ),
     );
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:rusconsign/detailPage/detail_page_binding.dart';
 import 'package:rusconsign/favoritePage/favorite_page.dart';
 import 'package:rusconsign/diskonPage/diskon_page.dart';
 import 'package:rusconsign/menu.dart';
+import 'package:rusconsign/notificationPage/notifcation_page.dart';
 import 'package:rusconsign/profilePage/profile_page.dart';
 import '../authentication/page/login_page.dart';
 import '../authentication/page/register_page.dart';
@@ -19,14 +20,13 @@ List<GetPage> routes = [
   GetPage(name: "/menu", page: () => const Menu()),
   GetPage(name: "/favoritepage", page: () => const FavoritePage()),
   GetPage(name: "/forgotpasswordpage", page: () => const ForgotPasswordPage()),
+  GetPage(name: "/profilepage", page: () => const ProfilePage()),
+  GetPage(name: "/diskonpage", page: () => const DiskonPage()),
+  GetPage(name: "/notificationpage", page: () => const NotificationPage()),
   GetPage(
       name: "/homepage", page: () => HomePage(), binding: HomePageBinding()),
   GetPage(
       name: "/detailpage",
       page: () => DetailPage(),
       binding: DetailPageBinding()),
-  
-  GetPage(name: "/profilepage", 
-  page: () => const ProfilePage()),
-  GetPage(name: "/diskonpage", page: () => const DiskonPage()),
 ];
