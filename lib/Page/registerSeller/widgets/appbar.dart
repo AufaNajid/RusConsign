7e2ba4dab14d0edd5c1c23ef0edf7875d29af21a
Zeyphrin <yakariayahya50@gmail.com>
2024-05-14 08:13:41 +0700

    fix [Y2] fix utils

--- lib/Page/registerSeller/widgets/appbar.dart
+++ lib/Page/registerSeller/widgets/appbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:get/get.dart';
 import 'package:rusconsign/utils/colors.dart';
+import 'package:rusconsign/utils/text_style.dart';
 
 class AppBarRegisterSeller extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -20,18 +21,14 @@ class AppBarRegisterSeller extends StatelessWidget implements PreferredSizeWidge
       surfaceTintColor: Colors.white,
       elevation: null,
       leading: IconButton(
-        icon: const Icon(FeatherIcons.chevronLeft),
+        icon: const Icon(FeatherIcons.chevronLeft, color: AppColors.borderIcon,),
         onPressed: () {
           Get.back();
         },
-        iconSize: 24,
       ),
       title: Text(
         title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
+        style: AppTextStyle().title(AppColors.titleLine)
       ),
       centerTitle: true,
     );
