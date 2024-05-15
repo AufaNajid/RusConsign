d5888e93763a7c06b03dff2ea13b0bf433021cfb
ZykX <Zikrinaya05@gmail.com>
2024-05-15 12:05:44 +0700

    fix [MZ] fix user profile page

---
+++ lib/Page/userProfilePage/widgets/button_icon_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rusconsign/utils/app_responsive.dart';
+import 'package:rusconsign/utils/colors.dart';
+import 'package:rusconsign/utils/text_style.dart';
+
+class ButtonIconWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+
+  const ButtonIconWidget({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: AppResponsive().screenWidth(context) * 0.09,
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color: AppColors.activeIconType,
+        ),
+        onPressed: () {},
+        style: ButtonStyle(
+          shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          backgroundColor: const MaterialStatePropertyAll(AppColors.button1),
+        ),
+        label: Text(
+          title,
+          style: AppTextStyle().subHeader(AppColors.textButton1),
+        ),
+      ),
+    );
+  }
+}
