279bd5f5500efe46accb3b47f6877afa6351c23e
Zeyphrin <yakariayahya50@gmail.com>
2024-05-21 12:41:53 +0700

    feat [Y2] checkpont

--- lib/Page/cartPage/widgets/product_card.dart
+++ lib/Page/cartPage/widgets/product_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:get/get.dart';
 import 'package:rusconsign/utils/colors.dart';
 import 'package:rusconsign/utils/extension.dart';
@@ -22,99 +24,120 @@ class ProductCardCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Card(
-            color: AppColors.cardProdukTidakDipilih,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                child: Row(
-                      children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        child: Column(
+          children: [
+            Card(
+              color: AppColors.cardProdukTidakDipilih,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Row(
+                        children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        imagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Gatungan Kunci (Key Chain) Boneka unik",
+                        style: AppTextStyle().descriptionBold(AppColors.titleLine),
+                      ),
+                      Row(
+                          children: [
+                        SizedBox(
+                          height: 25,
+                          width: 25,
+                          child: ClipOval(
+                            child: Image.network(
+                              profileImagePath,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          sellerUsername,
+                          style: AppTextStyle().textInfo(AppColors.description),
+                        ),
+                      ].withSpaceBetween(width: 5)),
+                      Row(
+                        children: [
+                          const Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: AppColors.bintang,
+                                size: 18,
+                              ),
+                              Icon(
+                                Icons.star_border_rounded,
+                                color: AppColors.borderIcon,
+                                size: 20,
+                              ),
+                            ],
+                          ),
+                          Text(
+                            rating.toString(),
+                            style: AppTextStyle().textInfoBold(AppColors.description),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '${'total'.tr} :',
+                            style: AppTextStyle().textInfo(AppColors.description),
+                          ),
+                          Text(
+                            'Rp $price',
+                            style: AppTextStyle().textInfoBold(AppColors.hargaStat),
+                          ),
+                        ],
+                      )
+                    ].withSpaceBetween(height: 5),
+                  ),
+                        ].withSpaceBetween(width: 5),
+                      ),
+                )),
+          Text(
+            'jumlah'.tr,
+            style: AppTextStyle().textInfo(AppColors.description),
+            textAlign: TextAlign.start,
+          ),
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                },
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      imagePath,
-                      fit: BoxFit.cover,
+                    borderRadius: BorderRadius.circular(3),
+                    child: Material(
+                      color: AppColors.button2,
+                      child: Center(child: Icon(FeatherIcons.plus, color: AppColors.textButton2, size: 20,))
                     ),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Gatungan Kunci (Key Chain) Boneka unik",
-                      style: AppTextStyle().descriptionBold(AppColors.titleLine),
-                    ),
-                    Row(
-                        children: [
-                      SizedBox(
-                        height: 25,
-                        width: 25,
-                        child: ClipOval(
-                          child: Image.network(
-                            profileImagePath,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        sellerUsername,
-                        style: AppTextStyle().textInfo(AppColors.description),
-                      ),
-                    ].withSpaceBetween(width: 5)),
-                    Row(
-                      children: [
-                        const Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: AppColors.bintang,
-                              size: 18,
-                            ),
-                            Icon(
-                              Icons.star_border_rounded,
-                              color: AppColors.borderIcon,
-                              size: 20,
-                            ),
-                          ],
-                        ),
-                        Text(
-                          rating.toString(),
-                          style: AppTextStyle().textInfoBold(AppColors.description),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          '${'total'.tr} :',
-                          style: AppTextStyle().textInfo(AppColors.description),
-                        ),
-                        Text(
-                          'Rp $price',
-                          style: AppTextStyle().textInfoBold(AppColors.hargaStat),
-                        ),
-                      ],
-                    )
-                  ].withSpaceBetween(height: 5),
-                ),
-                      ].withSpaceBetween(width: 5),
-                    ),
-              )),
-        Text(
-          'jumlah'.tr,
-          style: AppTextStyle().textInfo(AppColors.description),
-          textAlign: TextAlign.start,
+              ),
+              
+            ],
+          )
+          ],
         ),
-        
-        
-        ],
       ),
     );
   }
