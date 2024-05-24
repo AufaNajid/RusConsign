8571d31ce0cb2e9b25cdb177fc0ca4bb6dc7da86
Zeyphrin <yakariayahya50@gmail.com>
2024-05-24 13:41:16 +0700

    fix [Y2] fix countdown and button overflow

--- lib/Page/qrisPage/widgets/countdown.dart
+++ lib/Page/qrisPage/widgets/countdown.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:rusconsign/utils/app_responsive.dart';
 import 'package:rusconsign/utils/colors.dart';
@@ -60,70 +62,78 @@ class TimerTest extends StatelessWidget {
         );
 
         return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: AppResponsive().screenHeight(context)*0.090,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: AppColors.cardProdukTidakDipilih,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      hours,
-                      Text(
-                        'jam'.tr,
-                        style: AppTextStyle().subHeader(AppColors.description),
-                        textAlign: TextAlign.center
-                      )
-                    ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: AppColors.cardProdukTidakDipilih,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        hours,
+                        Text(
+                          'jam'.tr,
+                          style: AppTextStyle().subHeader(AppColors.description),
+                          textAlign: TextAlign.center
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-              Container(
-              height: AppResponsive().screenHeight(context)*0.090,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: AppColors.cardProdukTidakDipilih,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      minutes,
-                      Text(
-                        'menit'.tr,
-                        style: AppTextStyle().subHeader(AppColors.description),
-                        textAlign: TextAlign.center
-                      )
-                    ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                // height: AppResponsive().screenHeight(context)*0.2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: AppColors.cardProdukTidakDipilih,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        minutes,
+                        Text(
+                          'menit'.tr,
+                          style: AppTextStyle().subHeader(AppColors.description),
+                          textAlign: TextAlign.center
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-              Container(
-                height: AppResponsive().screenHeight(context)*0.090,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: AppColors.cardProdukTidakDipilih,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      seconds,
-                      Text(
-                        'detik'.tr,
-                        style: AppTextStyle().subHeader(AppColors.description),
-                        textAlign: TextAlign.center,
-                      )
-                    ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  // height: AppResponsive().screenHeight(context)*0.090,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: AppColors.cardProdukTidakDipilih,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        seconds,
+                        Text(
+                          'detik'.tr,
+                          style: AppTextStyle().subHeader(AppColors.description),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
