5bdebf526acc2e56be73922b543db33f36bf2333
Zeyphrin <yakariayahya50@gmail.com>
2024-05-27 18:24:42 +0700

    fix [Y2] fix file name

---
+++ lib/Page/profilePage/widgets/custom_button_profile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:get/get.dart';
+import 'package:rusconsign/utils/app_responsive.dart';
+import 'package:rusconsign/utils/colors.dart';
+import 'package:rusconsign/utils/text_style.dart';
+
+class ButtonProfile extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final String toPage;
+
+  const ButtonProfile({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.toPage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: AppResponsive().screenHeight(context) * 0.05,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            shape: const BeveledRectangleBorder()),
+        onPressed: () {
+          Get.toNamed(toPage);
+        },
+        child: Row(
+          children: [
+            Icon(icon, color: AppColors.description),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+            Expanded(
+              child: Text(
+                text,
+                style: AppTextStyle().description(AppColors.description),
+              ),
+            ),
+            Icon(
+              FeatherIcons.chevronRight,
+              color: AppColors.description,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Page/profilePage/widgets/pribadi_section_tabbar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:get/get.dart';
+import 'package:rusconsign/Page/profilePage/widgets/custom_button_profile.dart';
+import 'package:rusconsign/utils/colors.dart';
+
+
+
+class PribadiSection extends StatelessWidget {
+  const PribadiSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.cardIconFill,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            child: ButtonProfile(
+              icon: FeatherIcons.archive,
+              text: 'pembelianPJ'.tr,
+              toPage: "/orderpage",
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: AppColors.cardIconFill),
+            child: ButtonProfile(
+              icon: FeatherIcons.messageCircle,
+              text: 'chat'.tr,
+              toPage: "/chatlist",
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: AppColors.cardIconFill),
+            child: ButtonProfile(
+              icon: FeatherIcons.bell,
+              text: 'notifikasi'.tr,
+              toPage: "/notificationpage",
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              color: AppColors.cardIconFill,
+            ),
+            child: ButtonProfile(
+              icon: FeatherIcons.settings,
+              text: 'pengaturan'.tr,
+              toPage: "/settingpage",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
