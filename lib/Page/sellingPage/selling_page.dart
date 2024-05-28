9e860501fc624919a54b06187c36234523e594b8
Zeyphrin <yakariayahya50@gmail.com>
2024-05-28 08:39:51 +0700

    feat [Y2] selling page done

--- lib/Page/sellingPage/selling_page.dart
+++ lib/Page/sellingPage/selling_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:rusconsign/Page/sellingPage/selling_page_controller.dart';
 import 'package:rusconsign/Page/sellingPage/widgets/not_paid_card.dart';
+import 'package:rusconsign/Page/sellingPage/widgets/process_card.dart';
+import 'package:rusconsign/Page/sellingPage/widgets/review_card.dart';
 import 'package:rusconsign/Page/sellingPage/widgets/selling_appbar.dart';
 import 'package:rusconsign/Page/sellingPage/widgets/selling_filter_button.dart';
+import 'package:rusconsign/utils/app_responsive.dart';
 import 'package:rusconsign/utils/colors.dart';
 import 'package:rusconsign/utils/extension.dart';
+import 'package:rusconsign/utils/text_style.dart';
 
 class SellingPage extends StatelessWidget {
   final controller = Get.put(SellingPageController());
@@ -32,9 +36,13 @@ class SellingPage extends StatelessWidget {
                           icon: FeatherIcons.creditCard,
                           index: 0),
                       SellingFilterButton(
-                          text: 'proses'.tr, icon: FeatherIcons.clock, index: 1),
+                          text: 'proses'.tr,
+                          icon: FeatherIcons.clock,
+                          index: 1),
                       SellingFilterButton(
-                          text: 'selesai'.tr, icon: FeatherIcons.check, index: 2),
+                          text: 'selesai'.tr,
+                          icon: FeatherIcons.check,
+                          index: 2),
                       SellingFilterButton(
                           text: 'ulasan'.tr, icon: FeatherIcons.star, index: 3),
                       SellingFilterButton(
@@ -42,43 +50,114 @@ class SellingPage extends StatelessWidget {
                     ].withSpaceBetween(width: 10),
                   ),
                 ),
-            //     NotPaidCardSelling(
-            //   imagePath: 'https://via.placeholder.com/100x100',
-            //   title: 'dadadwndandadjad',
-            //   profileImagePath: 'https://via.placeholder.com/40x40',
-            //   profileName: 'bagas Prasetyo',
-            //   rating: 5.0,
-            //   price: 15000,
-            //   quantity: 2,
-            // ),
-                
-               Obx(() {
-              if (controller.selectedIndex == 0) {
-                return ListView.separated(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                shrinkWrap: true,
-                itemCount: 10,
-                 separatorBuilder: (BuildContext context, int index) {
-                return SizedBox(height: 10); // Atur jarak sesuai kebutuhan Anda
-              },
-                 itemBuilder: (BuildContext context, int index) {
-                  return             NotPaidCardSelling(
-              imagePath: 'https://via.placeholder.com/100x100',
-              title: 'dadadwndandadjad',
-              metodePembayaran: "COD",
-              lokasiPertemuan: "Basina Food",
-              rating: 5.0,
-              price: 15000,
-              quantity: 2,
-            );
-              });
-              } else {
-                return Center(
-                  child: Text('Halaman belum diimplementasikan'),
-                );
-              }
-            }),
-            
+                Obx(() {
+                  if (controller.selectedIndex == 0) {
+                    return ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(height: 10);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return const NotPaidCardSelling(
+                          imagePath: 'https://via.placeholder.com/100x100',
+                          title: 'belom bayar',
+                          metodePembayaran: "COD",
+                          lokasiPertemuan: "Basina Food",
+                          rating: 5.0,
+                          price: 15000,
+                        );
+                      },
+                    );
+                  } else if (controller.selectedIndex == 1) {
+                    return ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(height: 10);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return const ProcessCardSelling(
+                          imagePath: 'https://via.placeholder.com/100x100',
+                          title: 'proses',
+                          metodePembayaran: "COD",
+                          lokasiPertemuan: "Basina Food",
+                          rating: 5.0,
+                          price: 15000,
+                        );
+                      },
+                    );
+                  } else if (controller.selectedIndex == 2) {
+                    return ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(height: 10);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return const ProcessCardSelling(
+                          imagePath: 'https://via.placeholder.com/100x100',
+                          title: 'udah bayar',
+                          metodePembayaran: "COD",
+                          lokasiPertemuan: "Basina Food",
+                          rating: 5.0,
+                          price: 15000,
+                        );
+                      },
+                    );
+                  } else if (controller.selectedIndex == 3) {
+                    return ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(height: 10);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return ReviewCardSelling(
+                          imagePath: 'https://via.placeholder.com/100x100',
+                          username: "Agus Spakbor",
+                          rating: 4.5,
+                          date: DateTime.now(),
+                          desc:
+                              "Ampar ampar sigma, Sigma ku fanum tax Gyatt sebiji Dihurung skibidi Mangga rizzler mangga rizzler Mewing kai cenat Ligma dimakan sussy Apinya gooning mogging",
+                          like: 156,
+                          disLike: 12,
+                        );
+                      },
+                    );
+                  } else {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: AppResponsive().screenHeight(context) * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/images/fluent--box-search-24-regular.svg",
+                            // ignore: deprecated_member_use
+                            color: AppColors.hargaStat,
+                            width: AppResponsive().screenWidth(context) * 0.1,
+                            height: AppResponsive().screenHeight(context) * 0.1,
+                          ),
+                          Text(
+                            'belumAdaPesanan'.tr,
+                            style:
+                                AppTextStyle().subHeader(AppColors.description),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                })
               ],
             ),
           ),
